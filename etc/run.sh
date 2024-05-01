62ede078ad404f3d66f38d1ff3b913fb2c0129b7
Luan Fernandes <luan.fernandes@buser.com.br>
2024-05-01 15:35:26 -0300

    some toml examples and remove comments

--- etc/run.sh
+++ etc/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# echo "Current directory: $(pwd)"
-# echo "Listing the current directory contents:"
-# ls -l
-
-echo "PYTHONPATH: $PYTHONPATH"
 
 python dialog/make_embeddings.py
 uvicorn dialog.app.server:app --host 0.0.0.0 --port 8080 --reload
