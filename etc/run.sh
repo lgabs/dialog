f3096b00b6743259a93c5388a16d92240dd4a57c
Luan Fernandes <luan.fernandes@buser.com.br>
2024-05-02 22:17:17 -0300

    add skip embeddings env var

--- etc/run.sh
+++ etc/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-python dialog/make_embeddings.py
+if [ "$REBUILD_VECTORDB" == "true" ]; then
+    echo "Running make_embeddings.py"
+    python dialog/make_embeddings.py
+else
+    echo "Skipping make_embeddings.py"
+fi
+
 uvicorn dialog.app.server:app --host 0.0.0.0 --port 8080 --reload
--- etc/run_evals.sh
+++ etc/run_evals.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-python dialog/make_embeddings.py
+if [ "$REBUILD_VECTORDB" == "true" ]; then
+    echo "Running make_embeddings.py"
+    python dialog/make_embeddings.py
+else
+    echo "Skipping make_embeddings.py"
+fi
+
 python evals/evals.py
