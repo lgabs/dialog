f9af9c94ebe7370d64ff0c9dedbc2166745ce58b
Luan Fernandes <luan.fernandes@buser.com.br>
2024-05-03 10:24:46 -0300

    more tests for a sample dataset evals

--- Makefile
+++ Makefile
@@ -4,12 +4,13 @@ build-api:
 build-db:
 	docker compose build db
 
-run-evals:
+evals:
 	docker run --rm --name dialog-evals \
 	-p 8081:8081 \
 	--env-file .env \
 	-v "./src:/app" \
 	-v "./data:/app/data" \
 	-v "./etc:/app/etc" \
+	--network=dialog_default \
 	dialog-api \
 	/app/etc/run_evals.sh
--- src/evals/evals.py
+++ src/evals/evals.py
@@ -1,6 +1,7 @@
 import os
 from typing import List, Dict
 import json
+from operator import itemgetter
 
 import openai
 
@@ -8,42 +9,36 @@ from langsmith import Client
 from langsmith.wrappers import wrap_openai
 from langchain.smith import RunEvalConfig
 
-from dialog.chains.rag_with_history import rag_with_history_chain
+from dialog.chains.rag import rag_chain
 
 EVALS_PATH = os.environ["EVALS_PATH"]
+DATASET_NAME = os.environ["DATASET_NAME"]
+
 
 def read_openai_eval_examples(path: str) -> List[Dict]:
     """Parses evaluation samples when they are OpenAI's format for evals."""
-    with open(path, 'r') as jsonl_file:
+    with open(path, "r") as jsonl_file:
         jsonl_list = list(jsonl_file)
     return [json.loads(jline) for jline in jsonl_list]
 
 
 if __name__ == "__main__":
-    # Choose a chain
-    chain = rag_with_history_chain
-
+    # Choose a chain (making some input/output change to match with private eval sample)
+    chain = (
+        {"question": itemgetter("user_message")}
+        | rag_chain
+        | {"text": itemgetter("output")}
+    )
     # Setup the evaluation
     client = Client()
-    dataset_name = "Q&A samples"
-    dataset = client.create_dataset(dataset_name, description="questions and answers.")
-    samples = read_openai_eval_examples(EVALS_PATH)
-    for sample in samples:
-        client.create_examples(
-            inputs={"question": sample["input"]},
-            outputs={"answer": sample["ideal"]},
-    dataset_id=dataset.id,
-    )
     # Choose and LLM
     openai_client = wrap_openai(openai.Client())
     # Choose evaluators
     eval_config = RunEvalConfig(
-        # We will use the chain-of-thought Q&A correctness evaluator
+        # We will use the chain-of-thought Q&A correctness as an example evaluator
         evaluators=["qa"],
     )
     results = client.run_on_dataset(
-    dataset_name=dataset_name, llm_or_chain_factory=chain, evaluation=eval_config
+        dataset_name=DATASET_NAME, llm_or_chain_factory=chain, evaluation=eval_config
     )
     project_name = results["project_name"]
-            
-            
