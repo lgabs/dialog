4c48dd1d674cc4ae35c6fcdfff61c76763fd432a
Luan Fernandes <luan.fernandes@buser.com.br>
2024-05-03 17:35:21 -0300

    no more necessary

--- examples/make_qa_example.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-raw_path = "examples/qa_example.csv"
-save_path = "data/knowledge_base.csv"
-
-df = pd.read_csv(raw_path, sep="\t")
-df["Document"] = "Question\n" + df["Question"] + "\n\nAnswer\n" + df["Answer"]
-
-df.to_csv(save_path, index=False)
